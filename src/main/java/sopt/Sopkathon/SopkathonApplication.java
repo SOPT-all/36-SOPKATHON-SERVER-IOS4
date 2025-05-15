2a700759025192cfc3d920fd51b85e4599c9e949
huncozyboy <leejh5838@naver.com>
2025-05-16 05:50:49 +0900

    chore : initial commit

---
+++ src/main/java/sopt/Sopkathon/SopkathonApplication.java
@@ -0,0 +1,13 @@
+package sopt.Sopkathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopkathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopkathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/Sopkathon/SopkathonApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.Sopkathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopkathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
