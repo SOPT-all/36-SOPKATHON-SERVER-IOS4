07aa2c142950ba42bef1d9901cb135d0889d16c9
huncozyboy <leejh5838@naver.com>
2025-05-18 04:01:57 +0900

    refactor : 유저 필드 함께반환

--- src/main/java/sopt/Sopkathon/domain/activity/dto/response/ActivitySummaryResponse.java
+++ src/main/java/sopt/Sopkathon/domain/activity/dto/response/ActivitySummaryResponse.java
@@ -1,16 +1,18 @@
 package sopt.Sopkathon.domain.activity.dto.response;
 
 
-import sopt.Sopkathon.domain.activity.entity.ActivityPost;
-
 import java.time.LocalDateTime;
+import sopt.Sopkathon.domain.activity.entity.ActivityPost;
 
 public record ActivitySummaryResponse(
         Long activityId,
         String title,
         String region,
         LocalDateTime startTime,
-        LocalDateTime endTime
+        LocalDateTime endTime,
+        Long userId,    // 유저 이름 추가
+        String telephone
+
 ) {
     public static ActivitySummaryResponse from(ActivityPost post) {
         return new ActivitySummaryResponse(
@@ -18,7 +20,9 @@ public record ActivitySummaryResponse(
                 post.getTitle(),
                 post.getActivityRegion(),
                 post.getStartTime(),
-                post.getEndTime()
+                post.getEndTime(),
+                post.getWriter().getId(),
+                post.getWriter().getTelephone()
         );
     }
 }
--- src/main/java/sopt/Sopkathon/domain/member/controller/MemberController.java
+++ src/main/java/sopt/Sopkathon/domain/member/controller/MemberController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sopt.Sopkathon.domain.activity.dto.response.MyActivityListResponse;
+import sopt.Sopkathon.domain.activity.service.ActivityParticipationService;
 import sopt.Sopkathon.domain.member.dto.response.MemberResponse;
 import sopt.Sopkathon.domain.member.service.MemberService;
 import sopt.Sopkathon.global.common.dto.response.ApiResponse;
@@ -16,10 +18,19 @@ import sopt.Sopkathon.global.common.dto.response.ApiResponse;
 public class MemberController {
 
     private final MemberService memberService;
+    private final ActivityParticipationService participationService;
 
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<MemberResponse>> getMember(@PathVariable Long id) {
         MemberResponse member = memberService.getById(id);
         return ResponseEntity.ok(ApiResponse.success(200, "회원 조회 성공", member));
+    }
+
+    @GetMapping("/{memberId}/activities")
+    public ApiResponse<MyActivityListResponse> getMyActivities(
+            @PathVariable Long memberId
+    ) {
+        MyActivityListResponse response = participationService.getMyActivities(memberId);
+        return ApiResponse.success(200, "신청/참여 활동 조회 성공", response);
     }
 }
