4932e8614ee9b6a1f55d07ab237932b9da09897f
huncozyboy <leejh5838@naver.com>
2025-05-18 02:47:00 +0900

    feat : Builder.Default 어노테이션 추가

--- src/main/java/sopt/Sopkathon/domain/activity/entity/ActivityPost.java
+++ src/main/java/sopt/Sopkathon/domain/activity/entity/ActivityPost.java
@@ -1,7 +1,18 @@
 package sopt.Sopkathon.domain.activity.entity;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -61,9 +72,11 @@ public class ActivityPost extends BaseEntity {
     private ActivityTag tag;
 
     @OneToMany(mappedBy = "activityPost", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
     private List<ActivityParticipation> participations = new ArrayList<>();
 
     @OneToMany(mappedBy = "activityPost", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
     private List<ActivityReview> reviews = new ArrayList<>();
 
     @Column(nullable = false)
