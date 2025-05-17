1554aa455e787fadde69fb8b6d18305665adf4c3
huncozyboy <leejh5838@naver.com>
2025-05-18 01:55:33 +0900

    refactor : 불필요한 필드 삭제

--- src/main/java/sopt/Sopkathon/domain/member/dto/response/MemberResponse.java
+++ src/main/java/sopt/Sopkathon/domain/member/dto/response/MemberResponse.java
@@ -4,14 +4,12 @@ import sopt.Sopkathon.domain.member.entity.Member;
 
 public record MemberResponse(
         Long id,
-        String username,
         String telephone,
         String role
 ) {
     public static MemberResponse from(Member member) {
         return new MemberResponse(
                 member.getId(),
-                member.getUsername(),
                 member.getTelephone(),
                 member.getRole().name()
         );
