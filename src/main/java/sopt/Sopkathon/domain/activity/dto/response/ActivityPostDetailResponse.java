6fa776213d8a63dc3a1972e748f646b701a0146f
huncozyboy <leejh5838@naver.com>
2025-05-18 02:44:10 +0900

    feat : 반환데이터 구현

---
+++ src/main/java/sopt/Sopkathon/domain/activity/dto/response/ActivityPostDetailResponse.java
@@ -0,0 +1,27 @@
+package sopt.Sopkathon.domain.activity.dto.response;
+
+import java.time.LocalDateTime;
+import sopt.Sopkathon.domain.activity.entity.ActivityPost;
+import sopt.Sopkathon.domain.activity.entity.enums.ActivityTag;
+
+public record ActivityPostDetailResponse(
+        String title,
+        String content,
+        String subject,
+        String participationFee,
+        LocalDateTime startTime,
+        LocalDateTime endTime,
+        ActivityTag tag
+) {
+    public static ActivityPostDetailResponse from(ActivityPost post) {
+        return new ActivityPostDetailResponse(
+                post.getTitle(),
+                post.getContent(),
+                post.getSubject(),
+                post.getParticipationFee(),
+                post.getStartTime(),
+                post.getEndTime(),
+                post.getTag()
+        );
+    }
+}
