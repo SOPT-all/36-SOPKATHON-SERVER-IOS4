22dd6c56373d02f1b399f93d7728451086897546
huncozyboy <leejh5838@naver.com>
2025-05-18 00:48:13 +0900

    feat : 초기 엔티티 추가

---
+++ src/main/java/sopt/Sopkathon/domain/activity/entity/ActivityReview.java
@@ -0,0 +1,47 @@
+package sopt.Sopkathon.domain.activity.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import sopt.Sopkathon.domain.activity.entity.enums.ParticipationStatus;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+@Table(name = "activity_participation")
+public class ActivityReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private int star;
+
+//    @ManyToOne(optional = false)
+//    @JoinColumn(name = "member_id", nullable = false)
+//    private Member member;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "activity_post_id", nullable = false)
+    private ActivityPost activityPost;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ParticipationStatus status;
+}
